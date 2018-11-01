32e8047504693353cdb0d7a2c56d99393b102f19
sebastian schoof <mobscene_dude@hotmail.com>
2018-11-01 15:27:08 +0100

    Create Notifications

---
+++ Pi.Replicate.Processors/Notification/WorkEventData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pi.Replicate.Processing.Notification
+{
+    public class WorkEventData
+    {
+        public QueueKind QueueKind { get; set; }
+
+        public Type TypeOfData { get; set; }
+
+        public int CurrentWorkload { get; set; }
+    }
+}
