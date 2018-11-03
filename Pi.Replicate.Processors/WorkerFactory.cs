3ea7181586bd201855705f650397db305581221b
sebastian schoof <mobscene_dude@hotmail.com>
2018-11-03 09:47:33 +0100

    add efcore + factory for workers

---
+++ Pi.Replicate.Processors/WorkerFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pi.Replicate.Processing
+{
+    public class WorkerFactory : IWorkerFactory
+    {
+        public Worker CreateConsumerWorker(Type typeOfWorkData, QueueKind queueKind)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Worker CreateProducerWorker(QueueKind queueKind)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
