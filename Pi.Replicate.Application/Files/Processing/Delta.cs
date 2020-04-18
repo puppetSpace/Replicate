3eb6fc25d3362f6becc6cd1c90388fde05d0602c
Sebastian Schoof <mobscene_dude@hotmail.com>
2020-04-18 12:36:14 +0200

    add code to calculate and apply delta

--- Pi.Replicate.Application/Files/Processing/Delta.cs
+++ Pi.Replicate.Application/Files/Processing/Delta.cs
@@ -11,7 +11,7 @@ namespace Pi.Replicate.Application.Files.Processing
     //https://github.com/OctopusDeploy/Octodiff
     public class Delta
     {
-        
+
         public byte[] CreateSignature(Stream input)
         {
             var signatureBuilder = new SignatureBuilder();
@@ -27,5 +27,43 @@ namespace Pi.Replicate.Application.Files.Processing
                 return CreateSignature(fs);
         }
 
+        public byte[] CreateDelta(Stream input, byte[] signature)
+        {
+            var deltaStream = new MemoryStream();
+            var signatureReader = new SignatureReader(new MemoryStream(signature), null);
+            var deltaWriter = new AggregateCopyOperationsDecorator(new BinaryDeltaWriter(deltaStream));
+            var deltaBuilder = new DeltaBuilder();
+            deltaBuilder.BuildDelta(input, signatureReader, deltaWriter);
+
+            return deltaStream.ToArray();
+        }
+
+        public byte[] CreateDelta(string path, byte[] signature)
+        {
+            using (var fs = new FileStream(path, FileMode.Open))
+                return CreateDelta(fs, signature);
+        }
+
+        public void ApplyDelta(Stream input, byte[] delta, Stream output)
+        {
+            var deltaReader = new BinaryDeltaReader(new MemoryStream(delta), null);
+            var deltaApplier = new DeltaApplier { SkipHashCheck = true };
+            deltaApplier.Apply(input, deltaReader, output);
+        }
+
+        public void ApplyDelta(string path, byte[] delta)
+        {
+            var tempPath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), System.IO.Path.GetTempFileName());
+            using (var fs = new FileStream(path, FileMode.Open))
+            {
+                using (var fsout = new FileStream(tempPath, FileMode.Create))
+                {
+                    ApplyDelta(fs,delta,fsout);
+                }
+            }
+
+            //move temp to original
+            System.IO.File.Copy(tempPath,path,overwrite:true);
+        }
     }
 }
