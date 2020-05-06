c47c42bbcafdae7b5e3be55026d12ca00d88b8b7
Sebastian Schoof <sebastian.schoof@jandenul.com>
2020-05-06 15:42:34 +0200

    folder settings page

---
+++ Pi.Replicate.WebUi/Pages/Folders/FolderSettings.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pi.Replicate.WebUi.Pages.Folders
+{
+    public class FolderSettingsBase : ComponentBase
+    {
+		[Parameter]
+		public string FolderId { get; set; }
+
+		protected override Task OnInitializedAsync()
+		{
+			return base.OnInitializedAsync();
+		}
+	}
+}
