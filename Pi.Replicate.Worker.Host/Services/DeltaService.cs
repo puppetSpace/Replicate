ebcf1521c0bfe2e63e41254c1ac8a91d101fb24e
Sebastian Schoof <mobscene_dude@hotmail.com>
2020-05-30 15:40:34 +0200

    remove unused methods deltaservice

--- Pi.Replicate.Worker.Host/Services/DeltaService.cs
+++ Pi.Replicate.Worker.Host/Services/DeltaService.cs
@@ -16,82 +16,68 @@ namespace Pi.Replicate.Worker.Host.Services
 
 	public interface IDeltaService
 	{
-		ReadOnlyMemory<byte> CreateSignature(Stream input);
 		ReadOnlyMemory<byte> CreateSignature(string path);
-		ReadOnlyMemory<byte> CreateDelta(Stream input, ReadOnlyMemory<byte> signature);
 		ReadOnlyMemory<byte> CreateDelta(string path, ReadOnlyMemory<byte> signature);
-		void ApplyDelta(Stream input, ReadOnlyMemory<byte> delta, Stream output);
 		void ApplyDelta(string path, ReadOnlyMemory<byte> delta);
 	}
 
 	public class DeltaService : IDeltaService
-    {
+	{
 
-        public ReadOnlyMemory<byte> CreateSignature(Stream input)
-        {
-            var signatureBuilder = new SignatureBuilder();
-            var memoryStream = new MemoryStream();
-            var signatureWriter = new SignatureWriter(memoryStream);
-            signatureBuilder.Build(input, signatureWriter);
-            return memoryStream.ToArray();
-        }
-
-        public ReadOnlyMemory<byte> CreateSignature(string path)
-        {
+		public ReadOnlyMemory<byte> CreateSignature(string path)
+		{
 			using (var fs = System.IO.File.OpenRead(path))
-				return CreateSignature(fs);
-        }
+			{
+				var signatureBuilder = new SignatureBuilder();
+				var memoryStream = new MemoryStream();
+				var signatureWriter = new SignatureWriter(memoryStream);
+				signatureBuilder.Build(fs, signatureWriter);
+				return memoryStream.ToArray();
+			}
+		}
 
-        public ReadOnlyMemory<byte> CreateDelta(Stream input, ReadOnlyMemory<byte> signature)
-        {
-            var deltaStream = new MemoryStream();
-            var signatureReader = new SignatureReader(new MemoryStream(signature.ToArray()), LogProgressReporter.Get());
-            var deltaWriter = new AggregateCopyOperationsDecorator(new BinaryDeltaWriter(deltaStream));
-            var deltaBuilder = new DeltaBuilder();
-            deltaBuilder.BuildDelta(input, signatureReader, deltaWriter);
-
-            return deltaStream.ToArray();
-        }
-
-        public ReadOnlyMemory<byte> CreateDelta(string path, ReadOnlyMemory<byte> signature)
-        {
-            using (var fs = System.IO.File.OpenRead(path))
-                return CreateDelta(fs, signature);
-        }
-
-        public void ApplyDelta(Stream input, ReadOnlyMemory<byte> delta, Stream output)
-        {
-            var deltaReader = new BinaryDeltaReader(new MemoryStream(delta.ToArray()), LogProgressReporter.Get());
-            var deltaApplier = new DeltaApplier { SkipHashCheck = true };
-            deltaApplier.Apply(input, deltaReader, output);
-        }
-
-        public void ApplyDelta(string path, ReadOnlyMemory<byte> delta)
-        {
-            var tempPath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), System.IO.Path.GetTempFileName());
+		public ReadOnlyMemory<byte> CreateDelta(string path, ReadOnlyMemory<byte> signature)
+		{
 			using (var fs = System.IO.File.OpenRead(path))
+			{
+				var deltaStream = new MemoryStream();
+				var signatureReader = new SignatureReader(new MemoryStream(signature.ToArray()), LogProgressReporter.Get());
+				var deltaWriter = new AggregateCopyOperationsDecorator(new BinaryDeltaWriter(deltaStream));
+				var deltaBuilder = new DeltaBuilder();
+				deltaBuilder.BuildDelta(fs, signatureReader, deltaWriter);
 
+				return deltaStream.ToArray();
+			}
+
+		}
+
+		public void ApplyDelta(string path, ReadOnlyMemory<byte> delta)
+		{
+			var tempPath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), System.IO.Path.GetTempFileName());
+			using (var fs = System.IO.File.OpenRead(path))
 			{
 				using (var fsout = new FileStream(tempPath, FileMode.Create))
 				{
-					ApplyDelta(fs, delta, fsout);
+					var deltaReader = new BinaryDeltaReader(new MemoryStream(delta.ToArray()), LogProgressReporter.Get());
+					var deltaApplier = new DeltaApplier { SkipHashCheck = true };
+					deltaApplier.Apply(fs, deltaReader, fsout);
 				}
 			}
 
-            //move temp to original
-            System.IO.File.Copy(tempPath,path,overwrite:true);
-        }
+			//move temp to original
+			System.IO.File.Copy(tempPath, path, overwrite: true);
+		}
 
-        private class LogProgressReporter : IProgressReporter
-        {
-            public void ReportProgress(string operation, long currentPosition, long total)
-            {
-                Log.Verbose($"operation: {operation}, currentPosition: {currentPosition}, total: {total}");
-            }
+		private class LogProgressReporter : IProgressReporter
+		{
+			public void ReportProgress(string operation, long currentPosition, long total)
+			{
+				Log.Verbose($"operation: {operation}, currentPosition: {currentPosition}, total: {total}");
+			}
 
-            public static IProgressReporter Get() => new LogProgressReporter();
-        }
-    }
+			public static IProgressReporter Get() => new LogProgressReporter();
+		}
+	}
 
-    
+
 }
