082aa27ae82160cc3c9d168cff9cc8ddc511749d
Sebastian Schoof <sebastian.schoof@outlook.be>
2020-06-15 14:23:21 +0200

    add certificate section in appconfig + change way certificates are made

--- certificateImport.ps1
+++ certificateImport.ps1
@@ -1,5 +1,17 @@
-
-param([String] $password)
-$rootca = Import-PfxCertificate -FilePath 'PiRootCa.pfx' -CertStoreLocation Cert:\LocalMachine\Root -Confirm:$false -Password (ConvertTo-SecureString -AsPlainText $password -Force)
-#$rootca = Get-ChildItem cert:\localmachine\root | Where-Object {$_.Thumbprint -like "F50C910808B23C815B435D552EC044843F573C2D"}
+$params = @{
+  DnsName = "Pi Root Ca"
+  KeyLength = 2048
+  KeyAlgorithm = 'RSA'
+  HashAlgorithm = 'SHA256'
+  KeyExportPolicy = 'Exportable'
+  NotAfter = (Get-Date).AddYears(5)
+  CertStoreLocation = 'Cert:\LocalMachine\My'
+  KeyUsage = 'CertSign','CRLSign' #fixes invalid cert error
+}
+$rootCA = New-SelfSignedCertificate @params
 New-SelfSignedCertificate -DnsName $env:computername -KeyAlgorithm RSA -KeyLength 2048 -CertStoreLocation "cert:\LocalMachine\My" -Subject "CN=$env:computername" -Signer $rootca
+
+Export-Certificate -Cert $rootCA -FilePath "C:\rootCA.crt"
+Import-Certificate -CertStoreLocation 'Cert:\LocalMachine\Root' -FilePath "C:\rootCA.crt"
+
+Remove-Item "C:\rootCA.crt"
